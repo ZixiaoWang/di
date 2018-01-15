dab47c8c821cec3e34c327c0e69e6471a289672d
Jacky Wang <www.wangzixiao@gmail.com>
2018-01-15 19:17:39 +0800

    sth

--- Decorators/component.ts
+++ Decorators/component.ts
@@ -1,26 +1,14 @@
-import { ComponentConfig } from '../Core'
+import { ComponentConfig, ComponentStoreConfig } from '../Core/di.interface';
 import { InstanceStore } from "../Store";
 import { INSTANCE_STORE } from '../Store/instance.store'
 import { COMPONENT_STORE } from '../Store/component.store';
 
 export function Component(config: ComponentConfig) {
     return function(target) {
-        const strict = config.restrict || true;
-        const localInjector = new InstanceStore().add(config.provider || []);
-        let args = Reflect.getMetadata('design:paramtypes', target) || [];
-
-        args.forEach(paramType => {
-            let paramInstance = localInjector.get(paramType) || INSTANCE_STORE.get(paramType);
-            if(paramInstance === undefined && strict === true){
-                throw new Error(`Cannot find the instance of ${paramType.name}, please register ${paramType.name} to provider list`);
-            }else{
-                paramInstance = null;
-            }
-            args.push( paramInstance );
-        });
-
-        COMPONENT_STORE.register(target, args);
-
+        const strict = !!config.restrict;
+        const localInstanceStore: InstanceStore = new InstanceStore().add(config.provider || []);
+        let componentConfig: ComponentStoreConfig = { priority: 1, restrict: strict, instanceStore: localInstanceStore };
+        COMPONENT_STORE.register(target, componentConfig);
         return target;
     }
 }
